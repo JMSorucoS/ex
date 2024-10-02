9ec7244ec637ccf24f3f4dd6e076c775bbc7bae8
JMSorucoS <120837520+JMSorucoS@users.noreply.github.com>
2024-10-02 12:20:18 -0400

    Delete examen directory

--- examen/app/src/main/java/com/example/examen/MovieRomoteDataSource.kt
+++
@@ -1,9 +0,0 @@
-package com.example.examen
-
-class GithubRemoteDataSource(
-    val retrofiService: RetrofitBuilder
-) {
-    suspend fun getAvatarInfo(githubLogin: String): MovieResponseDto {
-        return retrofiService.apiService.getInfoMovie(githubLogin)
-    }
-}
